e298004074a3a9dc0a75f8d00862e7142095ec83
Andres Laguilavo <lagui2003@gmail.com>
2022-12-01 20:25:14 -0500

    bug porcentajes stats solucionado

--- src/components/Secundario/index.jsx
+++ src/components/Secundario/index.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import '../../styles/Secundario.css'
+import React from "react";
+import "../../styles/Secundario.css";
 
+export function Secundario({ infoPokemon }) {
+  let stats = infoPokemon.stats.map((a) => ({
+    stat: a.stat.name.split("-").join(" "),
+    value: a.base_stat,
+  }));
+  stats.shift();
 
-export function Secundario({infoPokemon}) {
-  let stats = infoPokemon.stats.map((a)=>({stat:a.stat.name.split('-').join(' '), value:a.base_stat,}))
-  stats.shift()
   return (
-    <div className='stats-container'>
-      {stats.map((a)=>(
-          <div className='stats-pokemons'>
+    <div className="stats-container">
+      {stats.map((a) => {
+        let porcentaje = a.value >= 100 ? 100 : a.value;
+        return (
+          <div className="stats-pokemons">
             <div></div>
-            <div style={{height:`${a.value}%`}}><p>{a.value}</p></div>
+            <div style={{ height: `${porcentaje}%` }}>
+              <p>{a.value}</p>
+            </div>
             <div>{a.stat}</div>
           </div>
-      ))}
-
+        );
+      })}
     </div>
-  )
+  );
 }
